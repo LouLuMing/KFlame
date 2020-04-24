bb00ff97919a5af1a62c76a8cf6df7e63ab10a19
lou.luming <18258448718@139.com>
2020-04-24 16:51:01 +0800

    修改nginx 穿透模式

--- src/com/china/fortune/nginx/HostSocket.java
+++
@@ -1,9 +0,0 @@
-package com.china.fortune.nginx;
-
-import java.nio.channels.SelectionKey;
-import java.nio.channels.SocketChannel;
-
-public class HostSocket {
-    public SelectionKey from;
-    public SocketChannel to;
-}
